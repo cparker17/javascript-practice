6cef3b9865c7c7bf1bd8d4f3687f41a9f2e40cd2
Chris Parker <chris@parkerdesignbuild.com>
2024-06-10 20:45:18 -0400

    part 3

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -6,3 +6,32 @@ function logger() {
 
 logger();
 logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    return `Juice with ${apples} apples and ${oranges} oranges`;
+}
+
+console.log(fruitProcessor(7, 5));
+
+
+//ARROW FUNCTIONS
+const birthYear = 1984;
+const calcAge3 = birthYear => 2037 - birthYear;
+console.log(calcAge3(birthYear));
+console.log("Age in 2037 will be " + calcAge3(birthYear) + ".");
+
+//ARRAYS
+const friends = ["Josh", "Andy", "Jeff"];
+console.log(friends);
+console.log(friends[0]);
+console.log(friends[1]);
+console.log(friends[2]);
+
+const years = new Array(1990, 1991, 2000, 2024);
+console.log(years);
+console.log(years.length);
+console.log(years.at(friends.length - 1));
+
+
+
