c98bd321226fbbb2787ffa8140b14ad0925bcbed
Chris Parker <chris@parkerdesignbuild.com>
2024-06-22 06:54:23 -0400

    section 9 started

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -2,28 +2,105 @@
 
 // Data needed for a later exercise
 const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+    '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
 const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+    name: 'Classico Italiano',
+    location: 'Via Angelo Tavanti 23, Firenze, Italy',
+    categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+    starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
+    order: function (starterIndex, mainIndex) {
+        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
     },
-    fri: {
-      open: 11,
-      close: 23,
+
+    orderDelivery: function (obj) {
+        console.log(obj);
     },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
+
+    openingHours: {
+        thu: {
+            open: 12,
+            close: 22,
+        },
+        fri: {
+            open: 11,
+            close: 23,
+        },
+        sat: {
+            open: 0, // Open 24 hours
+            close: 24,
+        },
     },
-  },
 };
+
+//OBJECT DESTRUCTURING
+const {name, openingHours, categories} = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+    name: restaurantName,
+    openingHours: hours,
+    categories: tags
+} = restaurant;
+
+//DEFAULT VALUES
+const {menu = [], starterMenu: starters = []} = restaurant;
+console.log(menu, starters);
+
+//MUTATING VARIABLES
+let l = 111;
+let m = 999;
+const obj = {l: 23, m: 7, n: 14};
+({l,m} = obj);
+console.log(l,m);
+
+
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+//ARRAY DESTRUCTURING
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+const [first, second] = restaurant.categories;
+console.log(first, second);
+const [one, , three] = restaurant.categories;
+console.log(one, three);
+
+const nested = [2, 4, [5, 6]];
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+//const ingredients = prompt("Let's make pasta!")
+
+const arr2 = [1,2,...[3,4]];
+const [r, s, ...others] = [1,2,3,4,5];
+console.log(a,b,others);
+
+console.log(3 || 'Chris');
+
+const rest1 = {
+    name: 'Capri',
+    numGuests: 0
+}
+const rest2 = {
+    name: 'La Piazza',
+    owner: 'Giovanni Rossi'
+}
+
+rest2.numGuests = rest2.numGuests || 10;
+console.log(rest2.numGuests);
+
+rest1.numGuests ||= 10;
+console.log(rest1.numGuests);
+
+const menu2 = [...restaurant.starterMenu, ...restaurant.mainMenu];
+for (const item of menu2) {
+    console.log(item);
+}
